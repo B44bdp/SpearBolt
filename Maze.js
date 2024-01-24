e96139d150d79712e5285c769e7ffec8e6c2524d
B44bdp <ltaylor4406@gmail.com>
2024-01-24 08:10:59 -0500

    Blue LED Checkpoint

--- Maze.js
+++ Maze.js
@@ -3,3 +3,14 @@
 // Date: 1.22.2024
 // Program: Maze
 // Bolt ID: 2Fae
+
+// Blue LED Checkpoint
+
+async function startProgram() {
+	await scrollMatrixText('Start', { r: 660, g: 560, b: 255 }, 15, true)
+	await speak("start")
+	await roll (-90, 55, 3.2)
+	setMainLed({ r: 0, g: 0, b: 90 })
+	
+	
+}
