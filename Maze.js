ed1b23e1802e2bfb25a010167d3cca89a5233bd7
B44bdp <ltaylor4406@gmail.com>
2024-01-25 08:40:14 -0500

    Red LED Code

--- Maze.js
+++ Maze.js
@@ -9,8 +9,18 @@
 async function startProgram() {
 	await scrollMatrixText('Start', { r: 660, g: 560, b: 255 }, 15, true)
 	await speak("start")
-	await roll (-90, 55, 3.2)
+	await roll (0 , 55, 2.7)
 	setMainLed({ r: 0, g: 0, b: 90 })
-	
-	
+	stopRoll(0)
+	await roll (90 , 50, 2.3)
+	await Sound.play()
+	await roll (180, 33, 2.5)
+	await roll(220, 28, 3.6,)
+	setMainLed({ r: 200, g: 0, b: 0 })
+
+
+
+
+
+
 }
